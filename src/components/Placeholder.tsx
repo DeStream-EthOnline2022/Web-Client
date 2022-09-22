6f3d7159c66474e59255e34592f6ff10795b5c94
Jeffrey <jeffreyhu16@gmail.com>
2022-09-22 20:53:48 +0800

    add homepage placeholder

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Placeholder() {
   return (
     <div className="placeholder-container">
-      
+      <div className="placeholder-content flex-center">
+        <h2 className='placeholder-header'>Access Livestream</h2>
+        <button className='placeholder-btn'>Go</button>
+      </div>
+      <div className='radial-gradient'></div>
     </div>
   );
 }
